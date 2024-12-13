be507e6f594df440aee12a00600b96d259e16b25
PatienceCrixix1z12 <nutsugahamapatience12@gmail.com>
2024-12-13 09:43:31 +0000

    add changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,41 @@
 // img.innerHTML ="<img src='../images/image (4).jpg"
 
 
-function changeButtonStyle(){
+// function changeButtonStyle(){
+//     let button = document.querySelector("button")
+//     button.style.backgroundColor = "blue";
+//    button.style.Color = "green";
+//    changeButtonStyle.innerHTML="hello";
+// }
+
+// let button = document.querySelector("button")
+// button.addEventListener("click", changeButtonStyle);
+
+
+// function changePaulstyle(){
+
+// }
+// let paul =document.querySelector("paul");  
+let button =document.querySelector("button");
+function openImage() {
+    let image = document.querySelector(".img")
+    image.style.display = "block"
+    let close = document.querySelector(".close")
+    close.style.display = "block"
     let button = document.querySelector("button")
-    button.style.backgroundColor = "blue";
-   button.style.Color = "green";
-   changeButtonStyle.innerHTML="hello";
+    button.style.display = "none"
+
+}
+button.addEventListener("click", openImage,);
+
+let close = document.querySelector(".close");
+function closeImage(){
+    let image = document.querySelector(".img")
+    image.style.display = "none"
+    let close = document.querySelector(".close")
+    close.style.display = "none"
+    let button = document.querySelector("button")
+    button.style.display = "block"
 }
 
-let button = document.querySelector("button")
-button.addEventListener("click", changeButtonStyle);
+close.addEventListener("click", closeImage);
